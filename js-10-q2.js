251aadb3cfbf33e88fdc03ea3ec7d81af5a60dbe
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-11-30 18:53:49 +0330

    2

---
+++ js-10-q2.js
@@ -0,0 +1,11 @@
+// Define any car using an object with 4 properties, like: car name, model, color and etc.
+// Then access the car’s name value in the console with the way you’ve learned
+
+//! Answer:
+let car = {
+    Name : "dena",
+    model : "camery",
+    color : "gray",
+    year : 2054
+}
+console.log(car.Name)
