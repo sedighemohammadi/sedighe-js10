6359c52ee1bcb4d80887d3b3421806c02d776490
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-11-30 18:53:43 +0330

    1

---
+++ js-10-q1.js
@@ -0,0 +1,4 @@
+// Using an example, explain the difference between block,global and function scope in variables.
+// Global Scope: Accessible from anywhere in the code.
+// - Function Scope: Accessible only within the function where it is declared.
+// - Block Scope: Accessible only within the block where it is declared (using let or const).
